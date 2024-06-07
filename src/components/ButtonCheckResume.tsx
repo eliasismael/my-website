4641134e2d7eb4b28afb0f061a7af97ad9c1c8ab
= <=>
2024-06-07 15:50:26 -0300

    new link to resume

--- src/components/ButtonCheckResume.tsx
+++ src/components/ButtonCheckResume.tsx
@@ -1,7 +1,7 @@
 export const ButtonCheckResume = () => (
   <button className="w-fit bg-gradient-to-tr from-blue-900 to-violet-900 px-6 py-2 rounded-3xl border-2 border-violet-600 mt-6 duration-500 shadow-white 20 shadow-sm text-white text-xl tracking-wider hover:border-violet-300">
     <a
-      href="https://drive.google.com/file/d/1wJ_MG208f4dKqdOSdMTh8OJA2w9iGCf3/view"
+      href="https://drive.google.com/file/d/1Z7n7elYA03nJVqZ6G88ADgXPwMHNPo5Y/view"
       target="_blank"
     >
       Check resume
