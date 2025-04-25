5a2980170f395b0477e5692f3b6a065979421464
Elías <eliasismael.pereyra@gmail.com>
2025-04-25 17:27:10 -0300

    added undr exp

--- src/components/Experience/constants/experienceInfo.ts
+++ src/components/Experience/constants/experienceInfo.ts
@@ -1,20 +1,32 @@
 import imgBeAChampion from "../../../assets/img/experiencie/be-a-champion-logo.jfif";
 import imgFrigate from "../../../assets/img/experiencie/frigate.jpg";
 import imgPrutopia from "../../../assets/img/experiencie/prutopia_logo.jfif";
+import imgUndr from "../../../assets/img/experiencie/undr.jfif";
 
 export const experienceInfo = [
   {
-    position: "Frontend Blockchain Developer",
+    position: "Frontend Web3 Developer",
+    img: imgUndr,
+    company: "UNDR Collective",
+    period: "March 2025 - April 2025",
+    link: "https://x.com/UNDRcrypto",
+    technologies: "Next.js - React.js - TypeScript - Privy - Alchemy SDK - PostgreSQL - Neon DB - Vercel - GIT - GitHub",
+    description:
+ 
+"I was primarily responsible for building the entire frontend based on Figma designs, ensuring a smooth experience across desktop and mobile. I developed reusable components that made API calls and interacted with databases. I regularly performed code refactoring to improve readability, maintainability, and performance. I also worked with SDKs such as Privy and Alchemy to support wallet-based session management and blockchain connectivity."
+    },
+  {
+    position: "Frontend Web3 Developer",
     img: imgPrutopia,
     company: "Prutopia",
-    period: "July 2024 - Present",
+    period: "July 2024 - October 2024",
     link: "https://www.prutopia.io/en",
     technologies: "Next.js - React.js - TypeScript - Web3 Auth - Wagmi - Figma",
     description:
       "Prutopia is a project that seeks to create a new resume and work experience management system taking advantage of blockchain technology to guarantee the decentralization and transferability of data. I work creating and implementing UI components, pages, routing, integrations with external API's along with interaction with virtual wallets and smart contracts on the blockchain.",
   },
   {
-    position: "Software Developer",
+    position: "Fullstack Developer",
     img: imgFrigate,
     company: "Frigate",
     period: "November 2023 - June 2024",
